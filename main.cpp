1121c2d86222230ff222eaaa747d371cd0209e53
visionsmika-gif <visionsmika@gmail.com>
2025-10-07 17:29:14 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,14 +1,5 @@
-/* INSTRUCTIONS:
-Amend the code as follows:
-Change the default constructor. Instead of it assigning null values, we want it to randomly select 3 or 4 legs, and to randomly select prices (including dollars and cents) from $100.00 to $999.99.
-Change the parameter constructor. Now it has just one parameter, the number of legs. Make it have two parameters: number of legs, and an array of 3 doubles (the prices).
-In the third code block (starting at line 67), amend this such that the default constructors are used to populate these objects.
-Exercise your changes in main() with clear, easy-to-understand output.
-
-Hint: random doubles? Sure! Try:
-
-const int MIN = 10000, MAX = 99999;
-double price = (rand() % (MAX-MIN+1) + MIN) / (double) 100; */
+// COMSC-210 | Lab 20 | Mika Aquino
+// IDE used: Visual Studio 2022
 
 #include <iostream>
 #include <iomanip>
@@ -27,16 +18,16 @@ public:
 	// Default constructor
 	Chair() {
 		prices = new double[SIZE];
-		// Randomly select 3 or 4 for legs
+		// Randomly select 3 or 4 legs
 		legs = (rand() % 2) + 3;
 		// Randomly select prices from $100.00 to $999.99.
 		const int MIN = 10000, MAX = 99999;
 		for (int i = 0; i < SIZE; ++i) {
-			prices[i] = (rand() % (MAX - MIN + 1) + MIN) / (double)100;
+			prices[i] = (rand() % (MAX - MIN + 1) + MIN) / (double) 100;
 		}
 	}
 	// Parameter constructor
-	Chair(int l, double p[]) {
+	Chair(int l, double p[]) {	// Takes number of legs and an array of 3 doubles (the prices)
 		prices = new double[SIZE];
 		legs = l;
 		for (int i = 0; i < SIZE; ++i) {
@@ -67,6 +58,7 @@ public:
 		cout << endl << endl;
 	}
 };
+
 int main() {
 	srand(static_cast<unsigned int>(time(0)));	// Seed for random numbers
 
@@ -87,13 +79,14 @@ int main() {
 	delete livingChair;
 	livingChair = nullptr;
 
-	// Create a dynamic array of chair objects using the default constructor to populate the objects
+	// Create a dynamic array of chair objects using the default constructor to populate the objects with random values
 	Chair* collection = new Chair[SIZE];
 	const int COLLECTION_SIZE = 3;
-
 	for (int i = 0; i < COLLECTION_SIZE; ++i) {
 		collection[i].print();
 	}
+	delete[] collection;
+	collection = nullptr;
 	
 	return 0;
 }
